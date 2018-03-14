e7efbfa0f6a6dafe540cf569c3d9f97dfa1c077e
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-14 13:57:50 +1100

    Add `move` function to Robot script
    Add a `move` function to the Robot script that will update the
    position of the Robot's `X` and `Y` properties by increasing their value
    by 1 or 0, depending on which direction it facing and if the next move
    does not place it outside or off the Table. (Robot's place off the table
    do not move).
    
    == Notes:
    - `rune` type is Golang's character type, however is a wrapper of the
      Unicode codepoint and can include emojis or UTF-8 characters that are
      often a bit more complex
    
    == References:
    - [The Go Programming Language Specification \- The Go Programming
      Language]
      (https://golang.org/ref/spec#Rune_literals)
    
    - [The Go Programming Language Specification \- The Go Programming
      Language]
      (https://golang.org/ref/spec#If_statements)

--- src/robot/robot.go
+++ src/robot/robot.go
@@ -8,6 +8,15 @@ type Robot struct {
     Facing string
 }
 
+var movement = func() map[string]map[rune]int {
+  return map[string]map[rune]int {
+    "NORTH": map[rune]int{'x': 0, 'y': 1},
+    "SOUTH": map[rune]int{'x': 0, 'y': -1},
+    "EAST": map[rune]int{'x': 1, 'y': 0},
+    "WEST": map[rune]int{'x': -1, 'y': 0},
+  }
+}
+
 var turn = func() map[string]map[string]string {
     return map[string]map[string]string {
         "NORTH": map[string]string{"LEFT": "WEST", "RIGHT": "EAST"},
@@ -27,4 +36,14 @@ func (r *Robot) left() {
 
 func (r *Robot) right() {
     r.Facing = turn()[r.Facing]["RIGHT"]
+}
+
+func (r *Robot) move() {
+    if x := r.X + movement()[r.Facing]['x']; x > 0 && x < 4 {
+        r.X = x
+    }
+
+    if y := r.Y + movement()[r.Facing]['y']; y > 0 && y < 4 {
+        r.Y = y
+    }
 }
--- src/robot/robot_test.go
+++ src/robot/robot_test.go
@@ -68,3 +68,83 @@ func TestRobotRightWhenFacingWest(t *testing.T) {
     subject.right()
     AssertEqual(t, subject.Facing, "NORTH")
 }
+
+func TestRobotMove(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "NORTH"}
+    subject.move()
+    AssertEqual(t, subject.X, 0)
+    AssertEqual(t, subject.Y, 1)
+    AssertEqual(t, subject.Facing, "NORTH")
+}
+
+func TestRobotMoveEast(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "EAST"}
+    subject.move()
+    AssertEqual(t, subject.X, 1)
+    AssertEqual(t, subject.Y, 0)
+    AssertEqual(t, subject.Facing, "EAST")
+}
+
+func TestRobotMoveSouth(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "SOUTH"}
+    subject.move()
+    AssertEqual(t, subject.X, 0)
+    AssertEqual(t, subject.Y, 0)
+    AssertEqual(t, subject.Facing, "SOUTH")
+}
+
+func TestRobotMoveWest(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "WEST"}
+    subject.move()
+    AssertEqual(t, subject.X, 0)
+    AssertEqual(t, subject.Y, 0)
+    AssertEqual(t, subject.Facing, "WEST")
+}
+
+func TestRobotMoveDoesNotFallOffTableAt04West(t *testing.T) {
+    subject := Robot{X: 0, Y: 4, Facing: "WEST"}
+    subject.move()
+    AssertEqual(t, subject.X, 0)
+    AssertEqual(t, subject.Y, 4)
+    AssertEqual(t, subject.Facing, "WEST")
+}
+
+func TestRobotMoveDoesNotFallOffTableAt04North(t *testing.T) {
+    subject := Robot{X: 0, Y: 4, Facing: "NORTH"}
+    subject.move()
+    AssertEqual(t, subject.X, 0)
+    AssertEqual(t, subject.Y, 4)
+    AssertEqual(t, subject.Facing, "NORTH")
+}
+
+func TestRobotMoveDoesNotFallOffTableAt44North(t *testing.T) {
+    subject := Robot{X: 4, Y: 4, Facing: "NORTH"}
+    subject.move()
+    AssertEqual(t, subject.X, 4)
+    AssertEqual(t, subject.Y, 4)
+    AssertEqual(t, subject.Facing, "NORTH")
+}
+
+func TestRobotMoveDoesNotFallOffTableAt44East(t *testing.T) {
+    subject := Robot{X: 4, Y: 4, Facing: "EAST"}
+    subject.move()
+    AssertEqual(t, subject.X, 4)
+    AssertEqual(t, subject.Y, 4)
+    AssertEqual(t, subject.Facing, "EAST")
+}
+
+func TestRobotMoveDoesNotFallOffTableAt40East(t *testing.T) {
+    subject := Robot{X: 4, Y: 0, Facing: "EAST"}
+    subject.move()
+    AssertEqual(t, subject.X, 4)
+    AssertEqual(t, subject.Y, 0)
+    AssertEqual(t, subject.Facing, "EAST")
+}
+
+func TestRobotMoveDoesNotFallOffTableAt40South(t *testing.T) {
+    subject := Robot{X: 4, Y: 0, Facing: "SOUTH"}
+    subject.move()
+    AssertEqual(t, subject.X, 4)
+    AssertEqual(t, subject.Y, 0)
+    AssertEqual(t, subject.Facing, "SOUTH")
+}
