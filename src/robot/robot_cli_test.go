034958d338e2af8ca6aeecb43b908b274669da50
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-14 16:47:04 +1100

    Add `Main` function to Robot package
    Add a `Main` function to the Robot package and update the `main.go`
    entrypoint accordingly. This Main function accepts the Operating System
    (OS) arguments, which is the compiled `main.go` file and any additional
    arguments after its is called, when the length of those arguments is
    more than just itself it will assume the first argument is a filename
    and proceed to use it as its Input source. Otherwise it will default to
    standard input `os.Stdin` and prompt the User for input via the console.
    
    == Notes:
    - Add several example files for testing purposes, as well for User to
      use with the package. All are based off the Toy Robot specification
    
    == References:
    - [testing \- Fill os\.Stdin for function that reads from it \- Stack
      Overflow]
      (https://stackoverflow.com/questions/46365221/fill-os-stdin-for-function-that-reads-from-it/46365584#46365584)
    
    - [unit testing \- How can I write a Go test that writes to stdin? \-
      Stack Overflow]
      (https://stackoverflow.com/questions/37256859/how-can-i-write-a-go-test-that-writes-to-stdin)
    
    - [go \- How can I pass a slice as a variadic input? \- Stack Overflow]
      (https://stackoverflow.com/questions/23723955/how-can-i-pass-a-slice-as-a-variadic-input)
    
    - [go \- Read line in golang \- Stack Overflow]
      (https://stackoverflow.com/questions/40035635/read-line-in-golang)
    
    - [go \- golang read line by line \- Stack Overflow]
      (https://stackoverflow.com/questions/30806288/golang-read-line-by-line)

---
+++ src/robot/robot_cli_test.go
@@ -0,0 +1,40 @@
+package robot
+
+import (
+    "testing"
+    . "test_helpers"
+)
+
+func TestRobotCLIMain(t *testing.T) {
+    subject := "REPORT\nEXIT\n"
+    osArgs := []string{"bin/main"}
+    output := MockStdin(subject, func() { Main(osArgs) })
+
+    AssertEqual(t, output, "0,0,NORTH\n")
+}
+
+func TestRobotCLIMainWithComplexInput(t *testing.T) {
+    subject := "PLACE 3,2,SOUTH\nREPORT\nMOVE\nRIGHT\nMOVE\nREPORT\nEXIT\n"
+    osArgs := []string{"bin/main"}
+    output := MockStdin(subject, func() { Main(osArgs) })
+
+    AssertEqual(t, output, "3,2,SOUTH\n2,1,WEST\n")
+}
+
+func TestRobotCLIMainWithExampleA(t *testing.T) {
+    subject := []string{"bin/main", "../../examples/example_a.txt"}
+    output := CaptureStdout(func() { Main(subject) })
+    AssertEqual(t, output, "0,0,NORTH\n")
+}
+
+func TestRobotCLIMainWithExampleB(t *testing.T) {
+    subject := []string{"bin/main", "../../examples/example_b.txt"}
+    output := CaptureStdout(func() { Main(subject) })
+    AssertEqual(t, output, "0,1,NORTH\n2,1,EAST\n")
+}
+
+func TestRobotCLIMainWithExampleC(t *testing.T) {
+    subject := []string{"bin/main", "../../examples/example_c.txt"}
+    output := CaptureStdout(func() { Main(subject) })
+    AssertEqual(t, output, "3,3,NORTH\n")
+}
