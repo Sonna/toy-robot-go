5f53d5e4590b524fd1401fa24358a61e40883645
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-14 13:42:07 +1100

    Add turn `left` & `right` to Robot
    Add the turn `left` and turn `right` methods to the Robot script, in
    order to update the robot struct `Facing` property to the new facing
    direction after executing one of the those commands; i.e.
    
    - When facing NORTH, turning left makes it face WEST,
    - When facing NORTH, turning right makes it face EAST,
    - etc
    
    == Notes:
    - Since only `Int` and `String` and be made immutable / constant the Map
      can be mutated when used, thus it is return of a global library
      function call to return a nested Map structure the same each time for
      single use.
    
    == References:
    - [Go by Example: Constants]
      (https://gobyexample.com/constants)
    
    - [Go by Example: Maps]
      (https://gobyexample.com/maps)
    
    - [go \- How to declare constant map in golang \- Stack Overflow]
      (https://stackoverflow.com/questions/18342195/how-to-declare-constant-map-in-golang)
    
    - [dictionary \- Nested maps in Golang \- Stack Overflow]
      (https://stackoverflow.com/questions/44305617/nested-maps-in-golang)

--- src/robot/robot.go
+++ src/robot/robot.go
@@ -8,6 +8,23 @@ type Robot struct {
     Facing string
 }
 
+var turn = func() map[string]map[string]string {
+    return map[string]map[string]string {
+        "NORTH": map[string]string{"LEFT": "WEST", "RIGHT": "EAST"},
+        "SOUTH": map[string]string{"LEFT": "EAST", "RIGHT": "WEST"},
+        "EAST": map[string]string{"LEFT": "NORTH", "RIGHT": "SOUTH"},
+        "WEST": map[string]string{"LEFT": "SOUTH", "RIGHT": "NORTH"},
+      }
+}
+
 func (r *Robot) report() {
     fmt.Printf("%d,%d,%s\n", r.X, r.Y, r.Facing)
+}
+
+func (r *Robot) left() {
+    r.Facing = turn()[r.Facing]["LEFT"]
+}
+
+func (r *Robot) right() {
+    r.Facing = turn()[r.Facing]["RIGHT"]
 }
--- src/robot/robot_test.go
+++ src/robot/robot_test.go
@@ -20,3 +20,51 @@ func TestRobotReport(t *testing.T) {
     output := CaptureStdout(subject.report)
     AssertEqual(t, output, "0,0,NORTH\n")
 }
+
+func TestRobotLeft(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "NORTH"}
+    subject.left()
+    AssertEqual(t, subject.Facing, "WEST")
+}
+
+func TestRobotLeftWhenFacingWest(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "WEST"}
+    subject.left()
+    AssertEqual(t, subject.Facing, "SOUTH")
+}
+
+func TestRobotLeftWhenFacingSouth(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "SOUTH"}
+    subject.left()
+    AssertEqual(t, subject.Facing, "EAST")
+}
+
+func TestRobotLeftWhenFacingEast(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "EAST"}
+    subject.left()
+    AssertEqual(t, subject.Facing, "NORTH")
+}
+
+func TestRobotRight(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "NORTH"}
+    subject.right()
+    AssertEqual(t, subject.Facing, "EAST")
+}
+
+func TestRobotRightWhenFacingEast(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "EAST"}
+    subject.right()
+    AssertEqual(t, subject.Facing, "SOUTH")
+}
+
+func TestRobotRightWhenFacingSouth(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "SOUTH"}
+    subject.right()
+    AssertEqual(t, subject.Facing, "WEST")
+}
+
+func TestRobotRightWhenFacingWest(t *testing.T) {
+    subject := Robot{X: 0, Y: 0, Facing: "WEST"}
+    subject.right()
+    AssertEqual(t, subject.Facing, "NORTH")
+}
